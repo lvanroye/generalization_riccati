c5fa2e9ecc5fbda934cf1192c35c4ce9249fe825
landerv <lander.vanroye@kuleuven.be>
2023-04-14 17:10:53 +0200

    work in progress

--- src/symspals/sparse_system_example.cpp
+++ src/symspals/sparse_system_example.cpp
@@ -5,14 +5,17 @@ int main()
     SparseLinearSystem ls = SparseLinearSystem();
     auto x = ls.variable(1);
     auto y = ls.variable(1);
-    ls.add_equation(x + y, Expression(1));
+    ls.add_equation(x + y, Const(1));
     auto sps = ls.get_sparsity();
     auto coeffs = ls.eval_coeffs();
+    
+    cout << "Sparsity pattern: " << endl;
     for(size_t i = 0; i<sps.size(); i++)
     {
         auto sp = sps[i];
         auto coeff = coeffs[i];
         cout <<  "(" <<  sp.row << ", " << sp.col << ") --> " << coeff << endl;
     }
+    cout << "rhs: " << endl;
     return 0;
 }
