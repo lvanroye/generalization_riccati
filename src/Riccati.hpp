2bd69545de91f43dc796e3609bd9bfbb33aac8ac
landerv <lander.vanroye@kuleuven.be>
2023-04-21 13:33:57 +0200

    create header files

--- src/Riccati.hpp
+++ src/Riccati.hpp
@@ -1,1 +1,5 @@
 // take in an ocp pointer and solve it
+// also include functionality for solve RHS and compute residual
+// make sure that all functionality is avaible though auxilary folder and not through fatrop, use fatrop only for setting up the problem
+
+
