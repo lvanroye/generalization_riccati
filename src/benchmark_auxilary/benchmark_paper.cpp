bd66f755c0bf3ca5ef434a59d25778bcee4d5ee0
landerv <lander.vanroye@kuleuven.be>
2023-05-08 17:16:40 +0200

    wip

--- src/benchmark_auxilary/benchmark_paper.cpp
+++ src/benchmark_auxilary/benchmark_paper.cpp
@@ -5,7 +5,7 @@ int main()
     vector<double> residu;
     BenchmarkHelperCP benchmark_helper_cp;
     benchmark_helper_cp.gen_riccati(true, solve_time, residu);
-    benchmark_helper_cp.sparse_solver("pardiso", solve_time, residu);
+    benchmark_helper_cp.sparse_solver("ma57", solve_time, residu);
     // // print the residu results
     // cout << "residu's: " << endl;
     // for (double r : residu)
@@ -16,8 +16,8 @@ int main()
     //     cout << t << endl;
 
     // BenchmarkHelperRandom benchmark_helper_rand;
-    // benchmark_helper_rand.gen_riccati(100, 10, 5, 10, 0, 10, true, true, solve_time, residu);
-    // benchmark_helper_rand.sparse_solver("ma57", 100, 10, 5, 10, 0, 10, solve_time, residu);
+    // benchmark_helper_rand.gen_riccati(100, 200, 100, 50, 50, 50, true, true, solve_time, residu);
+    // benchmark_helper_rand.sparse_solver("ma57", 100, 200, 100, 50, 50, 50, solve_time, residu);
     // print the residu results
     cout << "residu's: " << endl;
     for (double r : residu)
