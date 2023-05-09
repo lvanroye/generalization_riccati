df0ac77e858492f0ffc1999a03cdb221f9734fa8
landerv <lander.vanroye@kuleuven.be>
2023-05-09 15:38:15 +0200

    wip

--- src/benchmark_auxilary/benchmark_paper.cpp
+++ src/benchmark_auxilary/benchmark_paper.cpp
@@ -6,6 +6,9 @@ int main()
     BenchmarkHelperQuadrotor benchmark_helper_cp;
     benchmark_helper_cp.gen_riccati(false, solve_time, residu);
     benchmark_helper_cp.sparse_solver("ma57", solve_time, residu);
+    BenchmarkHelperRandom benchmark_helper_rand;
+    benchmark_helper_rand.gen_riccati(50, 10, 4, 10, 0, 10, false, false, solve_time, residu);
+    benchmark_helper_rand.sparse_solver("ma57",50, 10, 4, 10, 0, 10, solve_time, residu);
     cout << "residu's: " << endl;
     for (double r : residu)
         cout << r << endl;
