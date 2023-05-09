c5c43df851040d663d5330d33f8184a807e03e62
landerv <lander.vanroye@kuleuven.be>
2023-05-09 15:35:07 +0200

    small values initialization

--- src/benchmark_auxilary/benchmark_paper.cpp
+++ src/benchmark_auxilary/benchmark_paper.cpp
@@ -4,25 +4,11 @@ int main()
     vector<double> solve_time;
     vector<double> residu;
     BenchmarkHelperQuadrotor benchmark_helper_cp;
-    benchmark_helper_cp.gen_riccati(true, solve_time, residu);
+    benchmark_helper_cp.gen_riccati(false, solve_time, residu);
     benchmark_helper_cp.sparse_solver("ma57", solve_time, residu);
-    // // print the residu results
-    // cout << "residu's: " << endl;
-    // for (double r : residu)
-    //     cout << r << endl;
-    // // print the timings
-    // cout << "timings: " << endl;
-    // for (double t : solve_time)
-    //     cout << t << endl;
-
-    // BenchmarkHelperRandom benchmark_helper_rand;
-    // benchmark_helper_rand.gen_riccati(100, 200, 100, 50, 50, 50, true, true, solve_time, residu);
-    // benchmark_helper_rand.sparse_solver("ma57", 100, 200, 100, 50, 50, 50, solve_time, residu);
-    // print the residu results
     cout << "residu's: " << endl;
     for (double r : residu)
         cout << r << endl;
-    // print the timings
     cout << "timings: " << endl;
     for (double t : solve_time)
         cout << t << endl;
