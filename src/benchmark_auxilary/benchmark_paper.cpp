4d3c51af5a5337acf18ec8e6638343c10af5ff14
landerv <lander.vanroye@kuleuven.be>
2023-05-12 16:26:30 +0200

    wip

--- src/benchmark_auxilary/benchmark_paper.cpp
+++ src/benchmark_auxilary/benchmark_paper.cpp
@@ -3,11 +3,12 @@ int main()
 {
     vector<double> solve_time;
     vector<double> residu;
-    BenchmarkHelperQuadrotor benchmark_helper_cp;
-    benchmark_helper_cp.gen_riccati(false, solve_time, residu);
-    benchmark_helper_cp.sparse_solver("ma57", solve_time, residu);
-    // BenchmarkHelperRandom benchmark_helper_rand;
-    // benchmark_helper_rand.gen_riccati(50, 50, 25, 10, 10, 10, false, false, solve_time, residu);
+    // BenchmarkHelperQuadrotor benchmark_helper_cp;
+    BenchmarkHelperRandom benchmark_helper_rand;
+    for(int i =0; i<100; i++)
+    benchmark_helper_rand.gen_riccati(50, 50, 25, 10, 10, 10, true, false, solve_time, residu);
+    // benchmark_helper_cp.gen_riccati(true, solve_time, residu);
+    // benchmark_helper_cp.sparse_solver("ma57", solve_time, residu);
     // benchmark_helper_rand.sparse_solver("ma57",50, 10, 5, 10, 0, 10, solve_time, residu);
     cout << "residu's: " << endl;
     for (double r : residu)
